912b71332c5ada70030072df66187690bef1ceba
Minyoung <mgm94581@gmail.com>
2024-12-02 21:27:40 +0900

    수정

--- 1202-reactkiosk/src/App.jsx
+++ 1202-reactkiosk/src/App.jsx
@@ -3,11 +3,29 @@ import "./App.css";
 import MenuCard from "./components/MenuCard";
 import toffeeHot from "./assets/toffee-hot.png";
 import toffeeIce from "./assets/toffee-ice.png";
+import vinchaudHot from "./assets/vinchaud-hot.png";
+import vinchaudIce from "./assets/vinchaud-ice.png";
+import milkteaHot from "./assets/milktea-hot.png";
+import milkteaIce from "./assets/milktea-ice.png";
 
 // 가상 데이터
 export const COFFEE = [
   { id: 1, imgSrc: toffeeHot, name: "토피 넛 라떼", price: 5600 },
   { id: 2, imgSrc: toffeeIce, name: "아이스 토피 넛 라떼", price: 6100 },
+  {
+    id: 3,
+    imgSrc: vinchaudHot,
+    name: "논알코올 홀리데이 패션 티 뱅쇼",
+    price: 6100,
+  },
+  {
+    id: 4,
+    imgSrc: vinchaudIce,
+    name: "아이스 논알코올 홀리데이 패션 티 뱅쇼",
+    price: 6600,
+  },
+  { id: 5, imgSrc: milkteaHot, name: "클래식 밀크 티", price: 5200 },
+  { id: 6, imgSrc: milkteaIce, name: "아이스 클래식 밀크 티", price: 5700 },
 ];
 
 // 카드 형식의 이미지, 네임이 들어간 컴포넌트 만들어야됌
