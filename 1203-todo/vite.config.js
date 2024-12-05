730b4b43186fbf5092c44dc2f1406a3faf41be71
Minyoung <mgm94581@gmail.com>
2024-12-05 10:42:24 +0900

    배포 과정

--- 1203-todo/vite.config.js
+++ 1203-todo/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/goorm-practice/",
+});
+// 배포할 github pages 리포지토리 이름으로 변경, base값은 항상 슬래시로 시작하고 종료해야함
