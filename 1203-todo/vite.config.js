af019cb596fbc409bbf8725ff79e2c5a47c3e047
Minyoung <mgm94581@gmail.com>
2024-12-05 17:11:47 +0900

    배포 방법 변경으로 인한 삭제

--- 1203-todo/vite.config.js
+++ 1203-todo/vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/goorm-practice/1203-todo/",
+  base: "/goorm-practice/",
 });
 // 배포할 github pages 리포지토리 이름으로 변경, base값은 항상 슬래시로 시작하고 종료해야함
