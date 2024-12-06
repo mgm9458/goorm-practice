aa2b178f38e2a621d4c6692b8058df22ff1e87cc
Minyoung <mgm94581@gmail.com>
2024-12-06 09:04:27 +0900

    리액트 프로젝트 배포 설정

--- 1128-react1/vite.config.js
+++ 1128-react1/vite.config.js
@@ -4,5 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/goorm-practice/1128-react1/",
+  build: {
+    outDir: "../dist/1128-react1", // 상위 디렉토리의 dist/1128-react1에 빌드
+    emptyOutDir: false,
+  },
 });
 // 규칙을 정해주는?
--- 1129-counter/vite.config.js
+++ 1129-counter/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/goorm-practice/1129-counter/",
+  build: {
+    outDir: "../dist/1129-counter", // 상위 디렉토리의 dist/1129-counter에 빌드
+    emptyOutDir: false,
+  },
+});
