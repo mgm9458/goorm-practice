78c25b0d03d0ccae60963f81acfc2fbf161a5e5a
Minyoung <mgm94581@gmail.com>
2024-12-10 16:52:50 +0900

    할 일이 비어있을 경우 표시 추가

--- 1203-todo/src/components/TodoList.jsx
+++ 1203-todo/src/components/TodoList.jsx
@@ -1,20 +1,34 @@
 /* eslint-disable react/prop-types */
 import TodoItem from "./TodoItem";
+import emptyBox from "../assets/Group 1.svg";
 import styles from "../css/TodoList.module.css";
 
 const TodoList = ({ todos, updateTodo, toggleComplete, deleteTodo }) => {
   return (
-    <ul className={styles.todolist}>
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          updateTodo={updateTodo}
-          toggleComplete={toggleComplete}
-          deleteTodo={deleteTodo}
-        />
-      ))}
-    </ul>
+    <>
+      {todos.length === 0 ? (
+        <div className={styles.emptyState}>
+          <img
+            src={emptyBox}
+            alt="할 일이 없습니다."
+            className={styles.emptyImg}
+          />
+          <p className={styles.emptyText}>할 일이 없습니다. 추가해주세요!</p>
+        </div>
+      ) : (
+        <ul className={styles.todolist}>
+          {todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              updateTodo={updateTodo}
+              toggleComplete={toggleComplete}
+              deleteTodo={deleteTodo}
+            />
+          ))}
+        </ul>
+      )}
+    </>
     // ul 태그는 정렬되지 않은 목록을 나타내며 보통 불릿으로 표현.
   );
 };
